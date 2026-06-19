5935cc395f870b8294591e149c35dd6a8db5b5c7
Araadh <araadh3111@gmail.com>
2026-06-20 02:00:36 +0530

    advanced animations to landing page

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ function type() {
 }
 
 setTimeout(type, 2200);
+
+const frames = "⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏";
+let fi = 0;
+const spinner = document.querySelector(".spinner");
+
+setInterval(() => {
+    spinner.textContent = frames[fi];
+    fi = (fi + 1) % frames.length;
+}, 90);
+
+const face = document.querySelector(".face");
+
+setInterval(() => {
+    face.textContent = "(-‿-)";
+    setTimeout(() => face.textContent = "(◕‿◕)", 160);
+}, 2600);
